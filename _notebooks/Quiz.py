7b06fc699433f7f78cf143da3258a13bfb29c1fd
Tay Kim <taykimpro@gmail.com>
2022-08-26 14:56:06 -0700

    Making python quiz

--- _notebooks/Quiz.py
+++
@@ -1,8 +0,0 @@
-def Python_Math_Quiz(prompt):
-    return prompt [::-1] == prompt
-
-word = input ("Input a wor: ")
-if Python_Math_Quiz(word):
-    print("{} is a palindrome".format(word))
-else:
-    print("Not a palindrome")
